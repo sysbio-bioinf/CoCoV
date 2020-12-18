3c5083d70c1b0983c2e217b22add0d60c434eac9
Julian Schwab <julian.schwab@uni-ulm.de>
2020-12-18 13:17:53 +0100

    corrected view for chills and updated css

--- app/status_erfassen/slides/slideChills.js
+++ app/status_erfassen/slides/slideChills.js
@@ -1,5 +1,5 @@
 let slide = `
-<GridLayout id="chills" row="0" rows="auto, *, *">
+<GridLayout id="chills" row="0" rows="auto, *, *, *">
     <Label id="questionLabel" row="0" text="{{ chillsQuery }}" textWrap="true" fontSize="{{ mySize }}" class="questionText text-center" />
     <Button row="2" id="0" text="{{ chillsAnswer0 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad0 my-button-grad0:active capitalization" tap="{{ switchTab }}" />
     <Button row="3" id="1" text="{{ chillsAnswer1 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad1 my-button-grad1:active capitalization" tap="{{ switchTab }}" />
