d70db65a8a51aeb8643f5cc278fff1108bf7f7f3
azrael <azrael@eduroam-dynamic-clients.wlan.uni-ulm.de>
2020-12-18 11:58:22 +0100

    Texte und Buttons noch fertig anpassen

--- app/status_erfassen/slides/slideChills.js
+++ app/status_erfassen/slides/slideChills.js
@@ -1,11 +1,8 @@
 let slide = `
-<GridLayout id="chills" row="0" rows="auto, *, *, *, *, *, *">
+<GridLayout id="chills" row="0" rows="auto, *, *">
     <Label row="0" text="{{ chillsQuery }}" textWrap="true" fontSize="{{ mySize }}" class="questionText text-center" />
     <Button row="2" id="0" text="{{ chillsAnswer0 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad0 my-button-grad0:active capitalization" tap="{{ switchTab }}" />
     <Button row="3" id="1" text="{{ chillsAnswer1 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad1 my-button-grad1:active capitalization" tap="{{ switchTab }}" />
-    <Button row="4" id="2" text="{{ chillsAnswer2 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad2 my-button-grad2:active capitalization" tap="{{ switchTab }}" />
-    <Button row="5" id="2" text="{{ chillsAnswer3 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad3 my-button-grad3:active capitalization" tap="{{ switchTab }}" />
-    <Button row="6" id="2" text="{{ chillsAnswer4 }}" fontSize="{{ mySizeSmall }}" class="-btn my-button-grad4 my-button-grad4:active capitalization" tap="{{ switchTab }}" />
 </GridLayout>
 `;
 
